529cfb498c2368acdb2665cb013dfd8b54c5fb17
sasson <cha@MacBook-Air-de-Charlotte.local>
2020-06-11 21:53:50 +0200

    La récupération de films est possible

--- client/src/components/RechercheFilm.js
+++ client/src/components/RechercheFilm.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Interface.css";
 import { withKnobs } from "@storybook/addon-knobs";
 
 const RechercheFilm = () => {
   const input = React.createRef();
 
-  const handleSubmit = async (event) => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const triggerFetchAgain = () => setFetchAgain(!fetchAgain);
+
+  const fetchExample = async (event) => {
     event.preventDefault();
     const response = await fetch(
-      "https://154ggc17qg.execute-api.eu-west-1.amazonaws.com/dev/items/"+String( input.current.value ),
+      "https://154ggc17qg.execute-api.eu-west-1.amazonaws.com/dev/items/" + String(input.current.value)
     );
+    const responseJson =  await response.json();
+    setItems(responseJson);
   };
+
+  const displayMovie = () => {
+    return(
+      <ul>
+        {items.uuid}
+      </ul>);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
+    <div>
+      <form onSubmit={fetchExample}>
+        <label>
+          Name:
         <input type="text" ref={input} />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {displayMovie()}
+    </div>
   );
 };
-
 
 export default RechercheFilm;
